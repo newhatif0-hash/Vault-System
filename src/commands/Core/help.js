b2359e8b177b5d90a4a23ad369327f32f692615c
newhatif0-hash <newhatif0@gmail.com>
2026-09-07 02:17:41 +0100

    help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -125,7 +125,6 @@ export async function createInitialHelpMenu(client) {
     );
 
     const buttonRow = new ActionRowBuilder().addComponents([
-        bugReportButton,
         supportButton,
     ]);
 
