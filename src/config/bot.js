b8d4afe0364c9cf78807ea9e82a5f5189a209e01
newhatif0-hash <newhatif0@gmail.com>
2026-09-12 12:40:58 +0100

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "made by zeroprinc3",     // this is what people actually see
+        state: "made by r1gnok",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
