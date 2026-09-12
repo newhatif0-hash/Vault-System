25bf326b99069ab23dd2c26178790bfbf4a1b9d1
newhatif0-hash <newhatif0@gmail.com>
2026-09-12 16:24:48 +0100

    commandLoader.js

--- src/handlers/loaders/commandLoader.js
+++ src/handlers/loaders/commandLoader.js
@@ -62,7 +62,7 @@ export function registerCommands(client) {
                     registeredNames.add(shortcut);
                     const shortcutJson = {
                         ...commandJson,
-                        name: shortcut, // Change the name to the shortcut
+                        name: shortcut,
                     };
                     commands.push(shortcutJson);
                     logger.debug(`Registering shortcut: ${shortcut} -> ${commandName}`);
@@ -71,72 +71,6 @@ export function registerCommands(client) {
                 }
             }
         }
-
-        if (process.env.NODE_ENV !== 'production') {
-            logger.debug(`Registering command: ${commandName}`);
-        }
-    }
-
-    logger.info(`Collected ${commands.length} command payloads (${totalSubcommands} subcommands)`);
-    return { commands, totalSubcommands };
-}
-
-function getSubcommandInfo(commandJson) {
-    const subcommands = [];
-
-    if (commandJson.options && Array.isArray(commandJson.options)) {
-        for (const option of commandJson.options) {
-            if (option.type === 1 || option.type === 2) {
-                // Subcommand or SubcommandGroup
-                subcommands.push({
-                    name: option.name,
-                    type: option.type === 1 ? 'Subcommand' : 'SubcommandGroup',
-                });
-            }
-        }
-    }
-
-    return subcommands;
-}
-    logger.info(`✓ Loaded ${client.commands.size} commands`);
-}
-
-export function collectCommandPayloads(client) {
-    const commands = [];
-    let totalSubcommands = 0;
-    const registeredNames = new Set();
-
-    for (const command of client.commands.values()) {
-        const commandName = command.data.name;
-
-        if (registeredNames.has(commandName)) {
-            logger.warn(`Duplicate command name: ${commandName}`);
-            continue;
-        }
-
-        registeredNames.add(commandName);
-        const commandJson = command.data.toJSON();
-        commands.push(commandJson);
-        totalSubcommands += getSubcommandInfo(commandJson).length;
-
-        // ← START OF NEW CODE BLOCK
-        // Register shortcuts as separate commands
-        if (command.shortcuts && Array.isArray(command.shortcuts)) {
-            for (const shortcut of command.shortcuts) {
-                if (!registeredNames.has(shortcut)) {
-                    registeredNames.add(shortcut);
-                    const shortcutJson = {
-                        ...commandJson,
-                        name: shortcut, // Change the name to the shortcut
-                    };
-                    commands.push(shortcutJson);
-                    logger.debug(`Registering shortcut: ${shortcut} -> ${commandName}`);
-                } else {
-                    logger.warn(`Shortcut name already registered: ${shortcut}`);
-                }
-            }
-        }
-        // ← END OF NEW CODE BLOCK
 
         if (process.env.NODE_ENV !== 'production') {
             logger.debug(`Registering command: ${commandName}`);
